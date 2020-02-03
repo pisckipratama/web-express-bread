ad687b4183b78d70a61b0927db6318a8a7dfc420
Piscki Pratama <pisckipratama@gmail.com>
2020-02-03 20:14:16 +0700

    update fitur add

--- challange19/app.js
+++ challange19/app.js
@@ -1,23 +1,42 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 let parse = fs.readFileSync('data.json', 'utf8');
 let data = JSON.parse(parse);
 
 const app = express()
 
-// set ejs
+// set ejs and body parser
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
 
 // create main route
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
-    res.render('index', {data:data})
+    res.render('index', {
+        data: data
+    })
 });
 
 // create /add route
 app.get('/add', (req, res) => res.render('add'));
+app.post('/add', (req, res) => {
+    data.push({
+        id: data.length + 1,
+        string: req.body.string,
+        integer: req.body.integer,
+        float: req.body.float,
+        date: req.body.date,
+        boolean: req.body.boolean
+    });
+    fs.writeFileSync('data.json', JSON.stringify(data, null, 3));
+    res.redirect('/add');
+})
 
 // create /edit route
 app.get('/edit', (req, res) => res.render('edit'));
