fcffbf51e4044aad01c0a9795d0fb8ff857c000b
Piscki Pratama <pisckipratama@gmail.com>
2020-02-05 14:47:57 +0700

    add challange20 and fix show, edit features

---
+++ challange20/app.js
@@ -0,0 +1,73 @@
+// import dependencies
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3');
+
+// create express app 
+const app = express();
+
+// set template engine ejs
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// set bodyparser middleware
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+// setting db connection
+const dbName = path.join(__dirname, 'data', 'bread.db');
+const db = new sqlite3.Database(dbName, (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('successfully connection to the database.')
+})
+
+// create main route
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    const sql = `select * from data order by id`;
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            console.error(err.message);
+        }
+        res.render('index', {
+            model: rows
+        });
+    })
+});
+
+// create add route
+app.get('/add', (req, res) => res.render('add'));
+
+app.get('/edit/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = `select * from data where id = ?`;
+    db.get(sql, id, (err, row) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        res.render('edit', {
+            model: row
+        });
+    })
+})
+
+app.post('/edit/:id', (req, res) => {
+    const id = req.params.id;
+    const data = [req.body.string, parseInt(req.body.integer), parseFloat(req.body.float), req.body.date, req.body.boolean === 'true' ? 1 : 0, id];
+    const sql = `update data set string = ?, integer = ?, float = ?, date = ?, boolean = ? where (id = ?)`;
+    db.run(sql, data, (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        res.redirect('/');
+    })
+})
+
+app.listen(3000, () => {
+    console.log('server running on http://localhost:3000');
+})
---
+++ challange20/data.sql
@@ -0,0 +1,12 @@
+create table data
+(
+    id integer primary key auto increment,
+    string varchar(20),
+    integer numeric,
+    float real,
+    date text,
+    boolean integer
+);
+
+insert into data (string, integer, float, date, boolean) values ("mastering database", 18, 79.6, "2020-02-07", 1);
+update data set string = "mastering database", integer = 22, float = 78.9, date = "2020-02-06", boolean = 0 where (id = 3);
