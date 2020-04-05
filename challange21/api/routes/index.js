4c08a0e7bd32c7d5d7e9aad980c543f88b893cb6
Piscki F. Pratama <pisckipratama@gmail.com>
2020-04-05 21:37:25 +0700

    filter done

--- challange21/api/routes/index.js
+++ challange21/api/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const models = require('../models/index');
 const moment = require('moment');
+const Sequelize = require('sequelize')
 moment.locale('id');
 
 /* GET home page. */
@@ -9,27 +10,68 @@ router.get('/', (req, res, next) => {
   const limit = 3;
   const currentPage = parseInt(req.query.page) || 1;
   const offset = parseInt(currentPage - 1) * limit;
+  const op = Sequelize.Op
 
+  const { inputID, checkID, checkString, inputString, checkInteger, inputInteger, checkFloat, inputFloat, checkDate, startDate, endDate, checkBoolean, inputBoolean } = req.query;
   let filter = {}
 
+  if (checkID && inputID) {
+    filter.id = inputID;
+  }
+
+  if (checkString && inputString) {
+    filter.nama = {
+      [op.iLike]: `%${inputString}%`
+    }
+  }
+
+  if (checkInteger && inputInteger) {
+    filter.umur = inputInteger
+  }
+
+  if (checkFloat && inputFloat) {
+    filter.tinggi = inputFloat
+  }
+
+  if (checkDate && startDate && endDate) {
+    filter.tanggallahir = {
+      [op.between]: [startDate, endDate]
+    }
+  }
+
+  if (checkBoolean && inputBoolean) {
+    filter.menikah = inputBoolean
+  }
+
   models.Record.findAndCountAll({
+    where: filter,
     limit,
     offset
   }).then(result => {
     const totalPage = Math.ceil(result.count / limit);
     const url = req.url === '/' ? '/?page=1' : req.url;
     res.json({
-      result,
+      data: result,
       totalPage,
       url,
-      offset,
-      limit,
       currentPage,
-      query: req.query
     })
   })
     .catch(err => res.send(err))
 });
+
+router.get('/tes/:nama', (req, res, next) => {
+  const { nama } = req.params
+
+  models.Record.findAll({
+    where: {
+      nama: {
+        [op.iLike]: `%${nama}%`
+      }
+    }
+  }).then(result => res.json(result))
+    .catch(err => res.send(err))
+})
 
 router.post('/add', (req, res, next) => {
   const { nama, umur, tinggi, tanggallahir, menikah } = req.body;
